bcdedf157af9a257f9d303b3c1f15c1083eff030
Avantikakolte <avantikakolate@gmail.com>
2026-02-03 15:43:43 +0530

    Feet Measurement equality

---
+++ demo/src/test/java/com/apps/quantitymeasurementtest/QuantityMeasurementAppTest.java
@@ -0,0 +1,20 @@
+package com.apps.quantitymeasurementtest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class QuantityMeasurementAppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
