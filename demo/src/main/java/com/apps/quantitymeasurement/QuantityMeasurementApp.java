37d9efb409b375763ccb7c76d61e7b2cede8a2a7
Avantikakolte <avantikakolate@gmail.com>
2026-02-11 10:50:07 +0530

    Added quantity length

--- demo/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ demo/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,14 +2,11 @@ package com.apps.quantitymeasurement;
  
 import java.util.Scanner;
  
+import com.apps.quantitymeasurement.Length.LengthUnit;
+ 
 /**
  * Hello world!
  *
- */
- 
-/**
- * UC1: Feet Measurement Equality
- * Accepts user input and checks equality of two feet measurements.
  */
 public class QuantityMeasurementApp {
     // Inner class to represent Feet measurement
@@ -68,77 +65,78 @@ public class QuantityMeasurementApp {
         }
     }
  
-    public static void demonstrateFeetEquality(Scanner scanner) {
-        double value1 = 0;
-        double value2 = 0;
- 
-        // Taking first input
-        System.out.print("Enter first value in feet: ");
-        if (scanner.hasNextDouble()) {
-            value1 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
- 
-        // Taking second input
-        System.out.print("Enter second value in feet: ");
-        if (scanner.hasNextDouble()) {
-            value2 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
- 
-        Feet feet1 = new Feet(value1);
-        Feet feet2 = new Feet(value2);
- 
-        boolean result = feet1.equals(feet2);
- 
-        System.out.println("\nResult: Equal (" + result + ")");
+    // Length equality demonstration
+    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+        return length1.equals(length2);
     }
  
-    public static void demonstrateInchEquality(Scanner scanner) {
-        double value1 = 0;
-        double value2 = 0;
  
-        // Taking first input
-        System.out.print("Enter first value in inches: ");
-        if (scanner.hasNextDouble()) {
-            value1 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
+    //Helper method to read Length from user input
+ 
+    private static Length readLength(Scanner scanner) {
+        System.out.print("Enter length value: ");
+        double value = scanner.nextDouble();
+ 
+        System.out.print("Enter length unit (INCHES, FEET, YARDS, CENTIMETERS): ");
+        String unitInput = scanner.next().toUpperCase();
+        LengthUnit unit;
+        try {
+            unit = LengthUnit.valueOf(unitInput);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid length unit");
         }
  
-        // Taking second input
-        System.out.print("Enter second value in inches: ");
-        if (scanner.hasNextDouble()) {
-            value2 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
+        return new Length(value, unit);
  
-        Inches inches1 = new Inches(value1);
-        Inches inches2 = new Inches(value2);
+    }
  
-        boolean result = inches1.equals(inches2);
+    //Feet equality demonstration
+    // public static void demonstrateFeetEquality(Scanner scanner) {
+    //     Length length1 = readLength(scanner);
+    //     Length length2 = readLength(scanner);
  
-        System.out.println("\nResult: Equal (" + result + ")");
+    //     System.out.println("Input: Quantity 1: " + length1.getValue() + " " + length1.getUnit().name().toLowerCase() +
+    //             ", Quantity 2: " + length2.getValue() + " " + length2.getUnit().name().toLowerCase());
+       
+       
+ 
+    //     System.out.println("\nOutput: Equal (" + demonstrateLengthEquality(length1, length2) + ")");
+    // }
+ 
+    // public static void demonstrateInchEquality(Scanner scanner) {
+    //     Length length1 = readLength(scanner);
+    //     Length length2 = readLength(scanner);
+ 
+    //     System.out.println("Input: Quantity 1: " + length1.getValue() + " " + length1.getUnit().name().toLowerCase() +
+    //             ", Quantity 2: " + length2.getValue() + " " + length2.getUnit().name().toLowerCase());
+       
+       
+ 
+    //     System.out.println("\nOutput: Equal (" + demonstrateLengthEquality(length1, length2) + ")");
+ 
+       
+    // }
+ 
+    public static void demonstrateLengthComparison(Scanner scanner) {
+        Length lengthInFeet = readLength(scanner);
+        Length lengthInInches = readLength(scanner);
+ 
+        System.out.println("Input: Quantity 1: " + lengthInFeet.getValue() + " " + lengthInFeet.getUnit().name().toLowerCase() +
+                ", Quantity 2: " + lengthInInches.getValue() + " " + lengthInInches.getUnit().name().toLowerCase());
+ 
+        System.out.println("\nOutput: Equal (" + demonstrateLengthEquality(lengthInFeet, lengthInInches) + ")");
     }
  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try (scanner) {
-            System.out.println("Demonstrating Feet Equality:");
-            demonstrateFeetEquality(scanner);
- 
-            System.out.println("\nDemonstrating Inches Equality:");
-            demonstrateInchEquality(scanner);
-        }
-        finally {
+           
+            System.out.println("\nDemonstrating Length Comparison:");
+            demonstrateLengthComparison(scanner);
+        } finally {
             scanner.close();
         }
     }
 }
+ 
+ 
--- demo/src/test/java/com/apps/quantitymeasurementtest/QuantityMeasurementAppTest.java
+++ demo/src/test/java/com/apps/quantitymeasurementtest/QuantityMeasurementAppTest.java
@@ -1,8 +1,11 @@
 package com.apps.quantitymeasurementtest;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import com.apps.quantitymeasurement.Length;
+import com.apps.quantitymeasurement.Length.LengthUnit;
 
 /**
  * Unit test for simple App.
@@ -12,9 +15,130 @@ public class QuantityMeasurementAppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+   @Test
+    public void testEquality_YardToYard_SameValue() {
+        Length lengthInYard1 = new Length(3.0, LengthUnit.YARDS);
+        Length lengthInYard2 = new Length(3.0, LengthUnit.YARDS);
+        assertTrue(lengthInYard1.equals(lengthInYard2));    
     }
+    @Test
+    public void testEquality_YardToYard_differentValue(){
+        Length lengthInYard1 = new Length(3.0, LengthUnit.YARDS);
+        Length lengthInYard2 = new Length(4.0, LengthUnit.YARDS);
+        assertFalse(lengthInYard1.equals(lengthInYard2));      
+    }
+    @Test
+    public void testEquality_YardToFeet_EquivalentValue(){
+        Length lengthInYard = new Length(1.0, LengthUnit.YARDS);
+        Length lengthInFeet = new Length(3.0, LengthUnit.FEET);
+        assertTrue(lengthInYard.equals(lengthInFeet));  
+    }
+    @Test
+    public void testEquality_FeetToYard_SameValue(){
+        Length lengthInFeet = new Length(3.0, LengthUnit.FEET);
+        Length lengthInYard = new Length(1.0, LengthUnit.YARDS);
+        assertTrue(lengthInFeet.equals(lengthInYard));  
+ 
+    }
+    @Test
+    public void testEquality_YardToInches_EquivalentValue(){
+        Length lengthInYard = new Length(1.0, LengthUnit.YARDS);
+        Length lengthInInches = new Length(36.0, LengthUnit.INCHES);
+        assertTrue(lengthInYard.equals(lengthInInches));  
+    }      
+     @Test
+    public void testEquality_InchesToYard_EquivalentValue(){
+        Length lengthInInches = new Length(36.0, LengthUnit.INCHES);
+        Length lengthInYard = new Length(1.0, LengthUnit.YARDS);
+        assertTrue(lengthInInches.equals(lengthInYard));            
+    }
+ 
+    @Test
+    public void testEquality_YardToFeet_NotEquivalentValue() {
+        Length lengthInYard = new Length(2.0, LengthUnit.YARDS);
+        Length lengthInFeet = new Length(3.0, LengthUnit.FEET);
+        assertFalse(lengthInYard.equals(lengthInFeet));
+    }      
+ 
+    @Test
+    public void testEquality_CentimeterToInches_EquivalentValue(){
+        Length lengthInCentimeter = new Length(1, LengthUnit.CENTIMETERS);
+        Length lengthInInches = new Length(0.393701, LengthUnit.INCHES);
+        assertTrue(lengthInCentimeter.equals(lengthInInches));            
+    }      
+    @Test
+    public void testEquality_CentimeterToFeet_NonEquivalentValue(){
+        Length lengthInCentimeter = new Length(30.48, LengthUnit.CENTIMETERS);
+        Length lengthInFeet = new Length(1.0, LengthUnit.FEET);
+        assertFalse(lengthInCentimeter.equals(lengthInFeet));  
+    }
+    @Test
+    public void testEquality_MultiUnit_TransitiveProperty(){
+               Length Quantity1 = new Length(1.0, LengthUnit.YARDS); // 1 yards
+        Length Quantity2 = new Length(3.0, LengthUnit.FEET);  //
+        Length Quantity3 = new Length(36.0, LengthUnit.INCHES); // 36 inches
+        if(Quantity1.equals(Quantity2) && Quantity2.equals(Quantity3)){
+            assertTrue(Quantity1.equals(Quantity3));
+        } else {
+            assertFalse(true); // Force fail if transitive property does not hold
+        }  
+ 
+    }
+   
+    @Test(expected = IllegalArgumentException.class)
+    public void testEquality_YardWithNullUnit(){
+        Length lengthInYard1 = new Length(3.0, LengthUnit.YARDS);
+        Length lengthInYard2 = new Length(3.0, null);
+        assertFalse(lengthInYard1.equals(lengthInYard2));
+    }
+ 
+    @Test
+    public void testEquality_YardSameReference(){
+        Length lengthInYard1 = new Length(3.0, LengthUnit.YARDS);
+        Length lengthInYard2 = lengthInYard1;
+        assertTrue(lengthInYard1.equals(lengthInYard2));
+    }
+ 
+    @Test
+    public void testEquality_YardNullComparison(){
+        Length lengthInYard1 = new Length(3.0, LengthUnit.YARDS);
+        Length lengthInYard2 = null;
+        assertFalse(lengthInYard1.equals(lengthInYard2));
+    }
+ 
+    @Test(expected = IllegalArgumentException.class)
+    public void testEquality_CentimetersWithNullUnit(){
+        Length lengthInCentimeters = new Length(3.0, LengthUnit.CENTIMETERS);
+        Length lengthInCentimeters2 = new Length(3.0, null);
+        assertFalse(lengthInCentimeters.equals(lengthInCentimeters2));
+    }
+ 
+    @Test
+    public void testEquality_CentimetersSameReference(){
+        Length lengthInCentimeters1 = new Length(3.0, LengthUnit.CENTIMETERS);
+        Length lengthInCentimeters2 = lengthInCentimeters1;
+        assertTrue(lengthInCentimeters1.equals(lengthInCentimeters2));
+    }
+ 
+    @Test
+    public void testEquality_CentimetersNullComparison(){
+        Length lengthInCentimeters = new Length(3.0, LengthUnit.CENTIMETERS);
+        Length lengthInCentimeters2 = null;
+        assertFalse(lengthInCentimeters.equals(lengthInCentimeters2));
+    }
+ 
+    @Test
+    public void testEquality_AllUnits_ComplexScenario(){
+        Length Quantity1 = new Length(2.0, LengthUnit.YARDS); // 3 yards
+        Length Quantity2 = new Length(6.0, LengthUnit.FEET);  //
+        Length Quantity3 = new Length(72.0, LengthUnit.INCHES); // 36 inches
+        if(Quantity1.equals(Quantity2) && Quantity2.equals(Quantity3)){
+            assertTrue(Quantity1.equals(Quantity3));
+        // } else {
+        //     assertFalse(true); // Force fail if transitive property does not hold
+        }
+    }
+
+ 
+ 
 }
