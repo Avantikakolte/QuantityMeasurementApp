aefc6ab6591e8aeb4ffeab9529fa4ad6a3ce5883
Avantikakolte <avantikakolate@gmail.com>
2026-02-12 10:56:34 +0530

    Unit to Unit Conversion

--- demo/src/main/java/com/apps/quantitymeasurement/Length.java
+++ demo/src/main/java/com/apps/quantitymeasurement/Length.java
@@ -3,15 +3,13 @@ package com.apps.quantitymeasurement;
 public class Length {
     private final double value;
     private final LengthUnit unit;
-private static final double EPSILON = 0.0001;
+    private static final double EPSILON = 0.0001;  // Tolerance for floating-point comparison
  
     public enum LengthUnit {
         FEET(12.0),
         INCHES(1.0),
         YARDS(36.0),
-        CENTIMETERS(0.393701);
- 
-       
+        CENTIMETERS(0.393701);  // 1 cm = 0.393701 inches
  
         private final double conversionFactor;
  
@@ -45,6 +43,27 @@ private static final double EPSILON = 0.0001;
         return this.value * this.unit.getConversionFactor();
     }
  
+    //round to 2 decimal places
+    private static double round (double value)
+    {
+        return Math.round(value * 100.0) / 100.0;
+    }
+ 
+    //Method to convert Length to another unit
+    public Length convertTo(LengthUnit targetUnit)
+    {
+        if(targetUnit == null)
+        {
+            throw new IllegalArgumentException("Target unit cannot be null");
+        }
+        double valueInBaseUnit = this.convertToBaseUnit();
+        double convertedValue = valueInBaseUnit / targetUnit.getConversionFactor();
+        return new Length(round(convertedValue), targetUnit);
+    }
+ 
+ 
+    //Equality comparison method
+ 
     public boolean compare(Length thatLength)
     {
         if(thatLength == null)
@@ -66,7 +85,7 @@ private static final double EPSILON = 0.0001;
  
         Length other = (Length) obj;
  
-        return Double.compare(this.convertToBaseUnit(), other.convertToBaseUnit()) == 0;
+        return Math.abs(this.convertToBaseUnit() - other.convertToBaseUnit()) < EPSILON;
     }
  
  
@@ -76,6 +95,11 @@ private static final double EPSILON = 0.0001;
         return Double.hashCode(this.convertToBaseUnit());
     }
  
+    @Override
+    public String toString() {
+        return String.format("%.2f %s", value, unit.name());
+    }
+ 
     public double getValue() {
         return value;
     }
@@ -83,10 +107,31 @@ private static final double EPSILON = 0.0001;
         return unit;
     }
  
-    // standalone testing
+    //standalone testing
     // public static void main(String[] args) {
     //     Length l1 = new Length(12.0, LengthUnit.INCHES);
     //     Length l2 = new Length(1.0, LengthUnit.FEET);
     //     System.out.println("Are lengths equal? " + l1.compare(l2));
+ 
+    //     Length l3 = new Length(1.0, LengthUnit.YARDS);
+    //     Length l4 = new Length(36.0, LengthUnit.INCHES);
+    //     System.out.println("Are lengths equal? " + l3.compare(l4));
+ 
+    //     Length l5 = new Length(1.0, LengthUnit.CENTIMETERS);
+    //     Length l6 = new Length(0.393701, LengthUnit.INCHES);
+    //     System.out.println("Are lengths equal? " + l5.compare(l6));
+ 
+ 
+    //     Length l7 = new Length(1, LengthUnit.FEET);
+    //     Length l8 = new Length(30.48, LengthUnit.CENTIMETERS);
+    //     System.out.println("Are lengths equal? " + l7.compare(l8));
+ 
+ 
+    //     Length l9 = new Length(1, LengthUnit.FEET);
+    //     System.out.println("Converted: " + l9.convertTo(LengthUnit.CENTIMETERS));
+    //     Length l10 = new Length(91.44, LengthUnit.CENTIMETERS);
+    //     System.out.println("Are lengths equal? " + l9.compare(l10));
+ 
+       
     // }
-}
+}  
